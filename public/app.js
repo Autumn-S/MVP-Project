f4cae798f4ea1351ee513dede31e13435ffd2cfb
Autumn Sheridan <animalsoa@gmail.com>
2023-06-16 11:42:26 -0700

    update

--- public/app.js
+++ public/app.js
@@ -1,38 +1,39 @@
-document
-  .getElementById("characterForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
+.getElementById("characterForm")
+.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent form submission
 
-    const charName = document.getElementById("charName").value;
-    const charLevel = document.getElementById("charLevel").value;
-    const charClass = document.getElementById("charClass").value;
+  const charName = document.getElementById("charName").value;
+  const charLevel = document.getElementById("charLevel").value;
+  const charClass = document.getElementById("charClass").value;
 
-    const characterData = {
-      char_name: charName,
-      char_level: charLevel,
-      char_class: charClass,
-    };
+  const characterData = {
+    char_name: charName,
+    char_level: charLevel,
+    char_class: charClass,
+  };
 
-    fetch("/api/characters", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(characterData),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Error saving character.");
-        }
-      })
-      .then((data) => {
+  fetch("/api/characters", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(characterData),
+  })
+    .then((response) => {
+      if (response.ok) {
         alert("Character created!");
         document.getElementById("characterForm").reset();
-        console.log("characters data", data);
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-      });
-  });
+        // Retrieve updated character data after creation
+        fetch("/api/characters")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("characters data", data);
+          });
+      } else {
+        console.log("Error saving character.");
+      }
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+});
