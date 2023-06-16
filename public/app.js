706d8cbf606925318fb40de0f7c0db0589d977f5
Autumn Sheridan <animalsoa@gmail.com>
2023-06-16 11:42:53 -0700

    update

--- public/app.js
+++ public/app.js
@@ -1,39 +1,40 @@
-.getElementById("characterForm")
-.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent form submission
+document
+  .getElementById("characterForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
 
-  const charName = document.getElementById("charName").value;
-  const charLevel = document.getElementById("charLevel").value;
-  const charClass = document.getElementById("charClass").value;
+    const charName = document.getElementById("charName").value;
+    const charLevel = document.getElementById("charLevel").value;
+    const charClass = document.getElementById("charClass").value;
 
-  const characterData = {
-    char_name: charName,
-    char_level: charLevel,
-    char_class: charClass,
-  };
+    const characterData = {
+      char_name: charName,
+      char_level: charLevel,
+      char_class: charClass,
+    };
 
-  fetch("/api/characters", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(characterData),
-  })
-    .then((response) => {
-      if (response.ok) {
-        alert("Character created!");
-        document.getElementById("characterForm").reset();
-        // Retrieve updated character data after creation
-        fetch("/api/characters")
-          .then((res) => res.json())
-          .then((data) => {
-            console.log("characters data", data);
-          });
-      } else {
-        console.log("Error saving character.");
-      }
+    fetch("/api/characters", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(characterData),
     })
-    .catch((error) => {
-      console.log("Error:", error);
-    });
-});
+      .then((response) => {
+        if (response.ok) {
+          alert("Character created!");
+          document.getElementById("characterForm").reset();
+          // Retrieve updated character data after creation
+          fetch("/api/characters")
+            .then((res) => res.json())
+            .then((data) => {
+              console.log("characters data", data);
+            });
+        } else {
+          console.log("Error saving character.");
+        }
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  });
