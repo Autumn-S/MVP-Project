f675ba5764853b58f67a2585f9f76b915ff4fda6
Autumn Sheridan <animalsoa@gmail.com>
2023-06-17 14:32:02 -0700

    update

--- db/migration.sql
+++ db/migration.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS characters;
 
 CREATE TABLE characters(
-   char_id SERIAL PRIMARY KEY NOT NULL,
+   id SERIAL PRIMARY KEY NOT NULL,
    char_name VARCHAR(50) NOT NULL,
    char_level INT NOT NULL,
    char_class VARCHAR(30) NOT NULL
