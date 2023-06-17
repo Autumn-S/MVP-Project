8a96d33fda2a103564d70c0259c43cbb98ac9551
Autumn Sheridan <animalsoa@gmail.com>
2023-06-17 14:10:07 -0700

    update

--- public/app.js
+++ public/app.js
@@ -1,9 +1,44 @@
+// Function to create a character div
+function createCharacterDiv(characterData) {
+  const characterDiv = document.createElement("div");
+  characterDiv.classList.add("character");
+
+  const h2 = document.createElement("h2");
+  h2.textContent = characterData.char_name;
+
+  const p1 = document.createElement("p");
+  p1.textContent = `Level: ${characterData.char_level}`;
+
+  const p2 = document.createElement("p");
+  p2.textContent = `Class: ${characterData.char_class}`;
+
+  const updateButton = document.createElement("button");
+  updateButton.classList.add("update");
+  updateButton.textContent = "Update";
+  updateButton.addEventListener("click", handleUpdate);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", handleDelete);
+
+  characterDiv.appendChild(h2);
+  characterDiv.appendChild(p1);
+  characterDiv.appendChild(p2);
+  characterDiv.appendChild(updateButton);
+  characterDiv.appendChild(deleteButton);
+
+  return characterDiv;
+}
+
+// Function to toggle visibility of an element
 function toggleVisibility(elementId) {
   const element = document.getElementById(elementId);
   element.style.display = element.style.display === "none" ? "block" : "none";
   element.classList.toggle("centered");
 }
 
+// Event listeners for toggle links
 const aboutLink = document.getElementById("aboutLink");
 aboutLink.addEventListener("click", function () {
   toggleVisibility("aboutContainerBox");
@@ -19,6 +54,7 @@ displayLink.addEventListener("click", function () {
   toggleVisibility("characterContainerBox");
 });
 
+// Function to load and display character data
 function loadAndDisplayCharacterData() {
   fetch("/api/characters")
     .then((res) => res.json())
@@ -27,9 +63,8 @@ function loadAndDisplayCharacterData() {
       characterContainer.innerHTML = "";
 
       data.forEach((character) => {
-        const characterElement = document.createElement("div");
-        characterElement.textContent = `Name: ${character.char_name} Level: ${character.char_level} Class: ${character.char_class}`;
-        characterContainer.appendChild(characterElement);
+        const characterDiv = createCharacterDiv(character);
+        characterContainer.appendChild(characterDiv);
       });
     })
     .catch((error) => {
@@ -37,6 +72,50 @@ function loadAndDisplayCharacterData() {
     });
 }
 
+// Function to delete a character
+function deleteCharacter() {
+  fetch("/api/characters/:id", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(characterData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Character deleted successfully!");
+      } else {
+        console.log("Error deleting character.");
+      }
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+}
+
+// Function to update a character
+function updateCharacter() {
+  fetch("/api/characters/:id", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(characterData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Character updated successfully!");
+        console.log(characterData);
+      } else {
+        console.log("Error updating character.");
+      }
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+}
+
+// Function to handle form submission
 document
   .getElementById("characterForm")
   .addEventListener("submit", function (event) {
@@ -61,11 +140,15 @@ document
     })
       .then((response) => {
         if (response.ok) {
-          alert("Character created successfully!");
-          console.log(characterData);
+          return response.json();
         } else {
-          console.log("Error saving character.");
+          throw new Error("Error saving character.");
         }
+      })
+      .then((data) => {
+        const characterDiv = createCharacterDiv(data);
+        document.getElementById("characters").appendChild(characterDiv);
+        alert("Character created successfully!");
       })
       .catch((error) => {
         console.log("Error:", error);
