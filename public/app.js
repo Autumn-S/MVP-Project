68d6f00d69f72fc780a1940802c9f3d111dcd0c5
Autumn Sheridan <animalsoa@gmail.com>
2023-06-16 19:12:32 -0700

    update

--- public/app.js
+++ public/app.js
@@ -4,6 +4,16 @@ function toggleVisibility(elementId) {
   element.classList.toggle("centered");
 }
 
+const createLink = document.getElementById("createLink");
+createLink.addEventListener("click", function () {
+  toggleVisibility("formBox");
+});
+
+const updateLink = document.getElementById("updateLink");
+updateLink.addEventListener("click", function () {
+  toggleVisibility("characterContainerBox");
+});
+
 function loadAndDisplayCharacterData() {
   fetch("/api/characters")
     .then((res) => res.json())
@@ -22,40 +32,37 @@ function loadAndDisplayCharacterData() {
     });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  loadAndDisplayCharacterData(); // Fetch character data on page load
-  document
-    .getElementById("characterForm")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
+document
+  .getElementById("characterForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-      const charName = document.getElementById("charName").value;
-      const charLevel = document.getElementById("charLevel").value;
-      const charClass = document.getElementById("charClass").value;
+    const charName = document.getElementById("charName").value;
+    const charLevel = document.getElementById("charLevel").value;
+    const charClass = document.getElementById("charClass").value;
 
-      const characterData = {
-        char_name: charName,
-        char_level: charLevel,
-        char_class: charClass,
-      };
+    const characterData = {
+      char_name: charName,
+      char_level: charLevel,
+      char_class: charClass,
+    };
 
-      fetch("/api/characters", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(characterData),
+    fetch("/api/characters", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(characterData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Character created successfully!");
+          console.log(characterData);
+        } else {
+          console.log("Error saving character.");
+        }
       })
-        .then((response) => {
-          if (response.ok) {
-            alert("Character created successfully!");
-            console.log(characterData);
-          } else {
-            console.log("Error saving character.");
-          }
-        })
-        .catch((error) => {
-          console.log("Error:", error);
-        });
-    });
-});
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  });
