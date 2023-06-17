8f79173d85c10f98a28a7a4905a1a0941a101e91
Autumn Sheridan <animalsoa@gmail.com>
2023-06-16 18:46:49 -0700

    update

--- public/app.js
+++ public/app.js
@@ -45,11 +45,11 @@ function deleteCharacter() {
 function displayCharacterData(characterData) {
   const characterDisplay = document.getElementById("characterDisplay");
   characterDisplay.innerHTML = `
-        <h2>Character Information</h2>
-        <p><strong>Name:</strong> ${characterData.char_name}</p>
-        <p><strong>Level:</strong> ${characterData.char_level}</p>
-        <p><strong>Class:</strong> ${characterData.char_class}</p>
-      `;
+      <h2>Character Information</h2>
+      <p><strong>Name:</strong> ${characterData.char_name}</p>
+      <p><strong>Level:</strong> ${characterData.char_level}</p>
+      <p><strong>Class:</strong> ${characterData.char_class}</p>
+    `;
 }
 
 function loadAndDisplayCharacterData() {
@@ -62,11 +62,6 @@ function loadAndDisplayCharacterData() {
       data.forEach((character) => {
         const characterElement = document.createElement("div");
         characterElement.textContent = `Name: ${character.char_name} Level: ${character.char_level} Class: ${character.char_class}`;
-
-        characterElement.addEventListener("click", function () {
-          displayCharacterData(character);
-        });
-
         characterContainer.appendChild(characterElement);
       });
     })
@@ -77,13 +72,20 @@ function loadAndDisplayCharacterData() {
 
 document.addEventListener("DOMContentLoaded", function () {
   loadAndDisplayCharacterData(); // Fetch character data on page load
-
   document
     .getElementById("characterForm")
     .addEventListener("submit", function (event) {
       event.preventDefault();
 
-      // ... code for creating a character ...
+      const charName = document.getElementById("charName").value;
+      const charLevel = document.getElementById("charLevel").value;
+      const charClass = document.getElementById("charClass").value;
+
+      const characterData = {
+        char_name: charName,
+        char_level: charLevel,
+        char_class: charClass,
+      };
 
       fetch("/api/characters", {
         method: "POST",
